92bfcfb282331ad4990865433785430b7dda6b20
uliyana <uliyaiyaas@gmail.com>
2023-12-08 21:57:39 +0200

    Добавьте файлы проекта.

---
+++ Lab6,1Chernyshenko/Program.cs
@@ -0,0 +1,30 @@
+﻿class Program
+{
+    static bool Simm(string S, int i, int j)
+    {
+        if (i >= j)
+        {
+            return true;
+        }
+        if (S[i] != S[j])
+        {
+            return false;
+        }
+        return Simm(S, i + 1, j - 1);
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите строку:");
+        string S = Console.ReadLine();
+
+        Console.WriteLine("Введите индекс начала i:");
+        int i = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите индекс конца j:");
+        int j = Convert.ToInt32(Console.ReadLine());
+
+        bool isSymmetric = Simm(S, i, j);
+        Console.WriteLine("Симметричная ли часть строки S: " + isSymmetric);
+    }
+}
